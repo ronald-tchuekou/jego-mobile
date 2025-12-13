5bb8b123c9eedc9541918a5cede09b75cdcc2dba
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-12-13 23:56:32 +0100

    chore(build): update Babel/Metro configs and dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,14 +2,12 @@ module.exports = function (api) {
   api.cache(true)
 
   return {
-    presets: [['babel-preset-expo'], 'nativewind/babel'],
-
+    presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
     plugins: [
       [
         'module-resolver',
         {
           root: ['./'],
-
           alias: {
             '@': './',
             'tailwind.config': './tailwind.config.js',
