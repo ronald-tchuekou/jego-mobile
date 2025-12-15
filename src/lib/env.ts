8e726b3f4a9eeddc150916826223a0c5e1dc6f11
Ronald Tchuekou <ronaldtchuekou@gmail.com>
2025-12-15 19:36:52 +0100

    chore(env): update environment variables and env loader for Pusher support

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,6 +1,8 @@
 export const env = {
   API_URL: process.env.EXPO_PUBLIC_API_URL ?? 'http://localhost:3333',
   APP_URL: process.env.EXPO_PUBLIC_APP_URL ?? 'https://dev.jego.cm',
+  PUSHER_API_KEY: process.env.EXPO_PUBLIC_PUSHER_API_KEY ?? '',
+  PUSHER_CLUSTER: process.env.EXPO_PUBLIC_PUSHER_CLUSTER ?? '',
 }
 
 export function getApiUrl(path: string = '') {
